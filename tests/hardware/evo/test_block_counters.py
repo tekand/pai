fa03ea46b21bfbe6189b445a8c21e7470e80d453
Jevgeni Kiski <yozik04@gmail.com>
2020-08-23 19:23:30 +0300

    Some updates to tests.

--- tests/hardware/evo/test_block_counters.py
+++ tests/hardware/evo/test_block_counters.py
@@ -10,41 +10,47 @@ def test_1():
 
 
 def test_area_block_counter():
+    label_eeprom_address_start = 14955  # 0x03C17
+    next_label_after_bytes = 107  # 0x6B
+
     for i in range(0, 9):
-        i2 = ((i * 107) + 14955) // 256
-        print(data[i2])
+        i2 = ((i * next_label_after_bytes) + label_eeprom_address_start) // 256
+        print(f"{i}({i2}): {data[i2]}")
 
 
 def test_zone_block_counter():
+    label_eeprom_address_start = 0x00430
+    next_label_after_bytes = 16
+
     for i in range(0, 193):
-        i2 = ((i * 16) + 0x00430) // 256
-        print(f"{i}: {data[i2]}")
+        i2 = ((i * next_label_after_bytes) + label_eeprom_address_start) // 256
+        print(f"{i}({i2}): {data[i2]}")
 
 
 def test_module_block_counter():
     for i in range(0, 256):
-        i2 = ((i * 16) + 0x04E47) // 256
-        print(f"{i}: {data[i2]}")
+        i2 = ((i * 5) + 0x04E47) // 256
+        print(f"{i}({i2}): {data[i2]}")
 
 
 def test_user_block_counter():
     type_ = "evo"
-    for i in range(0, 1000):
+    for i in range(0, 257):
         if i < 257:
             i2 = ((i * 16) + 0x03E47) // 256
         else:
             i2 = (86416 if type_ == "evohd" else (i * 16) + 20880) // 256
         if i2 < len(data):
-            print(f"{i}: {data[i2]}")
+            print(f"{i}({i2}): {data[i2]}")
 
 
 def test_pgm_block_counter():
     for i in range(0, 32):
         i2 = ((i * 32) + 0x07082) // 256
-        print(f"{i}: {data[i2]}")
+        print(f"{i}({i2}): {data[i2]}")
 
 
 def test_door_block_counter():
     for i in range(0, 32):
         i2 = ((i * 16) + 0x0345C) // 256
-        print(f"{i}: {data[i2]}")
+        print(f"{i}({i2}): {data[i2]}")
